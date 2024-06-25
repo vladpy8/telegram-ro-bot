da0fb9c8fc1d73c0d63584b79840fcb4bc89453b
vladpy8 <vlpyslaru@gmail.com>
2024-06-25 23:30:11 +0300

    tests, basic

--- tests/vladpy_telegram_ro_bot/_application/_translator/_message_sentences_extractor_plain_text_test.py
+++ tests/vladpy_telegram_ro_bot/_application/_translator/_message_sentences_extractor_plain_text_test.py
@@ -1,19 +1,66 @@
 from vladpy_telegram_ro_bot._application._translator._message_sentences_extractor import MessageSentencesExtractor
 
 
+# TODO fix: tests
+# TODO fix: add tests
+
+
 def test_simplest() -> None:
 
 	sentences = (
 		MessageSentencesExtractor().extract(
 			message_text=('''
-				Предложение 1
-				Предложение 2
+				Sentence 1
+				Sentence 2.    Sentence 3!
+						Sentence 4   ?
+				Sentence 5, complex one
 			'''),
 			message_entities_dict=dict(),
 		)
 	)
 
-	assert sentences == [
-		'Предложение 1',
-		'Предложение 2',
-	]
+	assert (
+		sentences == [
+			'Sentence 1',
+			'Sentence 2.',
+			'Sentence 3!',
+			'Sentence 4   ?',
+			'Sentence 5, complex one',
+		]
+	)
+
+
+def test_date() -> None:
+
+	sentences = (
+		MessageSentencesExtractor().extract(
+			message_text=('''
+				Event happened on 2024.05.30, and then another event happened
+			'''),
+			message_entities_dict=dict(),
+		)
+	)
+
+	assert (
+		sentences == [
+			'Event happened on 2024.05.30, and then another event happened',
+		]
+	)
+
+
+def test_special_symbols() -> None:
+
+	sentences = (
+		MessageSentencesExtractor().extract(
+			message_text=('''
+				Sentence with special symbols littered across it
+			'''),
+			message_entities_dict=dict(),
+		)
+	)
+
+	assert (
+		sentences == [
+			'Sentence with special symbols littered across it',
+		]
+	)
---
+++ tests/vladpy_telegram_ro_bot/_application/_translator/_translator_utils_detect_language_test.py
@@ -0,0 +1,46 @@
+import textwrap
+
+from vladpy_telegram_ro_bot._application._translator._translator_utils import detect_target_language
+
+
+# TODO fix: tests
+# TODO fix: add tests
+
+
+def process_text(text: str,) -> str:
+
+	text = (
+		textwrap.dedent(text)
+		.replace('\n', ' ')
+		.strip(' \t\n\r')
+	)
+
+	return text
+
+
+def test_short() -> None:
+
+	assert (
+		detect_target_language(
+			language_code='ro',
+			message_text=(
+				process_text('''
+					Сafeaua este cea mai bună băutură
+				''')
+			)
+		)
+	)
+
+
+def test_short_mixed() -> None:
+
+	assert (
+		detect_target_language(
+			language_code='ro',
+			message_text=(
+				process_text('''
+					Bine. Bot este bine
+				''')
+			)
+		)
+	)
